911a9b32f363145d88550b4e89bf19c9ceafd29e
Danny Reyes <reyesdmusic@gmail.com>
2020-05-08 08:28:56 -0400

    db created, dependencies installed, and connection.js completed

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+INSERT INTO burgers (burger_name) 
+VALUES ("Double Cheeseburger");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Bacon and Gruyere");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Roquefort and Mushrooms");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Banh Mi Burger");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Cuban Frita with Guava Jam");
+
+INSERT INTO burgers (burger_name) 
+VALUES ("Smash Burger");
